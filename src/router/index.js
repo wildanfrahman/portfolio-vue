660270aded02ac260b784bfdb02163be0ae29473
Wildan <wildanmerahmerah@gmail.com>
2025-06-04 15:35:53 +0700

    gh-pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../pages/Home.vue";
 import Project from "../pages/Project.vue";
 import projectDetail from "../pages/projectDetail.vue";
@@ -14,7 +14,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
